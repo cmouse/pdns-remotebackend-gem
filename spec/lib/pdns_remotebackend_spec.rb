b4c2cad2a8b3722cbb56b0a0b110f39bdb0d1a7e
Aki Tuomi <cmouse@desteem.org>
2013-07-11 12:50:56 +0300

    Test skeleton

--- spec/lib/pdns_remotebackend_spec.rb
+++ spec/lib/pdns_remotebackend_spec.rb
@@ -1,2 +1,9 @@
 require 'spec_helper'
 
+describe PdnsRemotebackend::Handler, "#do_initialize" do
+  it "should return true for initialize" do
+     h = PdnsRemotebackend::Handler.new
+     h.do_initialize
+     h.result.should eq true
+  end
+end
